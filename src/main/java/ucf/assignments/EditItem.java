a60a58c08bba34bdc0f12a2cc7126675c0c9aea6
joshfeds14 <joshfeds14@gmail,com>
2021-07-25 17:05:01 -0400

    added copyright

--- src/main/java/ucf/assignments/EditItem.java
+++ src/main/java/ucf/assignments/EditItem.java
@@ -1,5 +1,8 @@
 package ucf.assignments;
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  Copyright 2021 Joshua Federman
+ */
 public class EditItem {
     public Item editName(Item item, String name){
         item.setName(name);
